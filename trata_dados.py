0ec98bfc437c921091a93e974e805f26fc8073db
eduardagoulart <goularteduarda.a@gmail.com>
2019-02-28 08:29:11 -0300

    separa diretorio e nome do site

--- trata_dados.py
+++ trata_dados.py
@@ -15,4 +15,28 @@ class ProcessaDadosURL:
         if len(url) > 1:
             url.pop(0)
             url = url[0]
-        print(url)
+        return url
+
+    def separa_nome_diretorio(self):
+        url = self.le_url()
+        tamanho_nome = 0
+        for i in range(len(url)):
+            tamanho_nome += 1
+            if i == '/':
+                break
+
+        nome_do_site = []
+        diretorio = []
+        for i in range(0, tamanho_nome):
+            if i < tamanho_nome:
+                nome_do_site.append(url[i])
+            else:
+                diretorio.append(url[i])
+
+        nome_do_site = ''.join(nome_do_site)
+        diretorio = ''.join(diretorio)
+
+        return nome_do_site, diretorio
+
+
+ProcessaDadosURL('http://www.presidentesjdr.com.br/linhasehorarios.html').separa_nome_diretorio()
