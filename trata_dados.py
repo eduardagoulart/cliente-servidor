15ba0748e6842bad3fb10fbe5f65e69355c60c49
eduardagoulart <goularteduarda.a@gmail.com>
2019-02-28 07:59:53 -0300

    usando orientação a objetos

--- trata_dados.py
+++ trata_dados.py
@@ -1,18 +1,18 @@
-"""
-Essa função testa se a URL passada é válida, ou seja, começa com http://
-"""
+class ProcessaDadosURL:
+    def __init__(self, url):
+        self.url = url
 
+    """
+    Essa função testa se a URL passada é válida, ou seja, começa com http://
+    """
 
-def le_url(url):
-    if "http://" not in url:
-        print("Site inválido, por favor tente outro")
-        exit(400)
-    url = url.split("http://")
+    def le_url(self):
+        if "http://" not in self.url:
+            print("Site inválido, por favor tente outro")
+            exit(400)
+        url = self.url.split("http://")
 
-    if len(url) > 1:
-        url.pop(0)
-        url = url[0]
-    print(url)
-
-
-le_url("http://www.presidentesjdr.com.br/")
+        if len(url) > 1:
+            url.pop(0)
+            url = url[0]
+        print(url)
