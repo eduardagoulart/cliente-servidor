609ba960257c5e3eac99d7a1a56570085a45566e
eduardagoulart <goularteduarda.a@gmail.com>
2019-03-05 15:26:39 -0300

    enviando mensagem para o servidor

--- cliente.py
+++ cliente.py
@@ -1,35 +1,26 @@
 import socket
 
+HOST = ''
+PORT = 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+
+print(f'Serving HTTP on PORT {PORT}')
+
+while True:
+    cliente_connection, cliente_address = listen_socket.accept()
+    request = cliente_connection.recv(1024)
+    print(f"Request: {request}")
+
+    http_response = """\
+HTTP/1.1 200 OK
+
+Hello, World!
 """
-HOST = '127.0.0.1'
-PORT = 80
+    http_response = str.encode(http_response)
+    cliente_connection.sendall(http_response)
+    cliente_connection.close()
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-
-print("1 to exit")
-
-msg = input()
-while msg != '1':
-    tcp.send(msg)
-    msg = input()
-
-print(tcp)
-tcp.close()
-"""
-
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-
-BUFFER_SIZE = 1024
-MESSAGE = "Hello!!"
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-
-data = s.recv(BUFFER_SIZE)
-s.close()
-
-print(f'received data: {data}')
