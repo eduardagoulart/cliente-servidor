3089356f69510a9d85b981968a496cdb2a97d6b1
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-05 15:47:55 -0300

    faltando aprenas a thread

--- teste_cliente.py
+++ teste_cliente.py
@@ -1,14 +1,61 @@
 import socket
-import sys
+import sys, os
 import select
 from trata_dados import ProcessaDadosURL
+
+
+def nome_sem_diretorio(url):
+    url = url.decode()
+    try:
+        p = url.index('www')
+        i = 5
+    except:
+        i = 0
+
+    nome = []
+    while url[i] != '.':
+        nome.append(url[i])
+        i = i + 1
+        if i >= len(url) - 1:
+            break
+
+    return ''.join(nome) + '.html'
+
+
+def nome_com_diretorio(path):
+    path = path.decode()
+    extensoes = ['.txt', '.mp3', '.pdf', '.html', '.jpg', '.png']
+    path = path.split('/')
+    for nome in path:
+        if len(nome) > 4:
+            if nome[-4:] in extensoes:
+                return nome
+
+    return "arquivo_generico"
+
+
+def analisa_erro(response):
+    erros = {"302": "302 Found",
+             "404": "404 Not Found",
+             "301": "301 Moved Permanently",
+             "401": "401 Unauthorized",
+             "400": "400 Bad Request",
+             "403": "403 Forbidden",
+             "500": "500 Internal Server Error",
+             "504": "504 Gateway Timeout",
+             "501": "501 Not Implemented",
+             "502": "502 Bad Gateway",
+             "503": "503 Service Unavailable"}
+
+    if response in erros.keys():
+        print(erros[response])
+        exit(1)
 
 
 def conecta(host, path):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, 80))
     host, path = host.encode(), path.encode()
-    print(path)
     s.sendall(b'GET /%b HTTP/1.1\r\nHOST: %b\r\n\r\n' % (path, host))
 
     reply = b''
@@ -22,10 +69,28 @@ def conecta(host, path):
     headers = reply.split(b'\r\n\r\n')[0]
     image = reply[len(headers) + 4:]
 
+    erro = headers.decode().split()
+    analisa_erro(erro[1])
+
+    if len(path) != 0:
+        nome_arq = nome_com_diretorio(path)
+
+        # diretorio = f'{os.mkdir(nome_arq[:-4])}/{nome_arq}'
+        arq_saida = open(nome_arq, 'wb')
+    else:
+        nome_arq = nome_sem_diretorio(host)
+        # diretorio = os.mkdir(nome_arq[:-4])
+        # print(type(diretorio))
+        # diretorio = str(diretorio) + nome_arq
+        arq_saida = open(nome_arq, 'wb')
+
+    arq_saida.write(image)
+    arq_saida.close()
+
     # save image
-    f = open('duda.html', 'wb')
-    f.write(image)
-    f.close()
+    # f = open('duda.jpg', 'wb')
+    # f.write(image)
+    # f.close()
 
 
 if '__main__' == __name__:
