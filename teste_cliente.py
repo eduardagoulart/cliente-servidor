359a2fb908891a5d6e2d3886c5d6a0e41c451cd0
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-05 14:02:45 -0300

    pegando dados do declado

--- teste_cliente.py
+++ teste_cliente.py
@@ -1,22 +1,44 @@
 import socket
+import sys
 import select
+from trata_dados import ProcessaDadosURL
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# http:///
-s.connect(('cenozoicforaminifera.com', 80))
-s.sendall(b'GET /sites/cenozoicforaminifera.com/files/styles/slideshow_large/public/Serra-Kiel.jpg?itok=bnHuaBhU HTTP/1.1\r\nHOST: cenozoicforaminifera.com\r\n\r\n')
 
-reply = b''
+def conecta(host, path):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, 80))
+    host, path = host.encode(), path.encode()
+    print(path)
+    s.sendall(b'GET /%b HTTP/1.1\r\nHOST: %b\r\n\r\n' % (path, host))
 
-while select.select([s], [], [], 3)[0]:
-    data = s.recv(2048)
-    if not data: break
-    reply += data
+    reply = b''
 
-headers =  reply.split(b'\r\n\r\n')[0]
-image = reply[len(headers)+4:]
+    while select.select([s], [], [], 3)[0]:
+        data = s.recv(2048)
+        if not data:
+            break
+        reply += data
 
-# save image
-f = open('image.jpg', 'wb')
-f.write(image)
-f.close()
+    headers = reply.split(b'\r\n\r\n')[0]
+    image = reply[len(headers) + 4:]
+
+    # save image
+    f = open('duda.html', 'wb')
+    f.write(image)
+    f.close()
+
+
+if '__main__' == __name__:
+    navegador = sys.argv[1]  # vai pegar a opção navegador ou servidor
+    url = sys.argv[2]
+    try:
+        porta = sys.argv[3]  # porta que será utilizada
+    except:
+        porta = 80
+
+    if navegador == 'navegador':
+        host, path = ProcessaDadosURL(url).separa_nome_diretorio()
+        conecta(host, path)
+    else:
+        print("Opção inválida!")
+        exit(404)
